506e990703cada6efdbef45f0f209d7d6e6ff8be
Eugine-Onyango <97959080+Eugine-Onyango@users.noreply.github.com>
2022-02-22 14:36:49 +0300

    Add files via upload

---
+++ library system/main.c
@@ -0,0 +1,21 @@
+/*
+project: county library management system.
+Author: Eugene Onyango
+Date: oct,2021
+Complier:GNU GCC
+Language: C99
+License: MIT
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{printf("\tCounty library\n");
+printf("Hello Mr. Elvis.\n");
+printf("what would you like to do?\n");
+printf("1. Add new user\n");
+printf("2. Add new book.\n");
+printf("selected action:\n");
+    return 0;
+}
